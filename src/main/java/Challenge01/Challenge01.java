48ad8605d4ebfc16ca04822cb97f14949b3a22ed
fponknevets <steven.knopf@gmail.com>
2019-03-24 09:31:29 +0000

    added first test which passes

---
+++ src/main/java/Challenge01/Challenge01.java
@@ -0,0 +1,10 @@
+package Challenge01;
+
+public class Challenge01 {
+
+    public String formatTime(int i) {
+
+        return "1 second";
+
+    }
+}
---
+++ src/test/java/Challenge01Tests.java
@@ -0,0 +1,20 @@
+
+import Challenge01.Challenge01;
+import org.junit.jupiter.api.Test;
+
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+
+
+public class Challenge01Tests {
+
+    @Test
+    void oneReturnsOneSecond(){
+        Challenge01 challenge01 = new Challenge01();
+
+        String result = challenge01.formatTime(1);
+
+        assertThat(result, equalTo("1 second"));
+    }
+
+}
