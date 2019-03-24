7e25e32a83614b1542e6ea52f22ee78bccdca13e
fponknevets <steven.knopf@gmail.com>
2019-03-24 14:01:33 +0000

    added ability to deal with years and days

--- src/main/java/Challenge01/Challenge01.java
+++ src/main/java/Challenge01/Challenge01.java
@@ -2,17 +2,37 @@ package Challenge01;
 
 public class Challenge01 {
 
-    final static int SECOND = 1;
-    final static int MINUTE = 60 * SECOND;
-    final static int HOUR = 60 * MINUTE;
-    final static int DAY = 24 * HOUR;
-    final static int YEAR = 365 * DAY;
+    public final static int SECOND = 1;
+    public final static int MINUTE = 60 * SECOND;
+    public final static int HOUR = 60 * MINUTE;
+    public final static int DAY = 24 * HOUR;
+    public final static int YEAR = 365 * DAY;
 
-    public String formatTime(int i) {
+    public String formatTime(int seconds) {
 
-        String result = Integer.toString(i) + " second";
-        result += i > 1 ? "s" : "";
-        
+        String result = "";
+
+        if ( seconds >= YEAR ) {
+            int yearcount = seconds / YEAR;
+            result += Integer.toString(yearcount) + " year";
+            result += yearcount > 1 ? "s" : "";
+            seconds -= yearcount * YEAR;
+        }
+
+        if ( seconds >= DAY) {
+            int daycount = seconds / DAY;
+            if ( result.length() > 0 ) { result += ", "; }
+            result += Integer.toString(daycount) + " day";
+            result += daycount > 1 ? "s" : "";
+            seconds -= daycount * DAY;
+        }
+
+        if ( seconds > 0 ){
+            if ( result.length() > 0 ) { result += " and "; }
+            result += Integer.toString(seconds) + " second";
+            result += seconds > 1 ? "s" : "";
+        }
+
         return result;
 
     }
--- src/test/java/Challenge01Tests.java
+++ src/test/java/Challenge01Tests.java
@@ -9,17 +9,38 @@ import static org.hamcrest.Matchers.*;
 public class Challenge01Tests {
 
     @Test
-    void oneReturnsOneSecondTest(){
+    void oneSecondTest(){
         Challenge01 challenge01 = new Challenge01();
         String result = challenge01.formatTime(1);
         assertThat(result, equalTo("1 second"));
     }
 
     @Test
-    void twoReturnsTwoSecondsTest(){
+    void twoSecondsTest(){
         Challenge01 challenge01 = new Challenge01();
         String result = challenge01.formatTime(2);
         assertThat(result, equalTo("2 seconds"));
+    }
+
+    @Test
+    void oneYearTest() {
+        Challenge01 challenge01 = new Challenge01();
+        String result = challenge01.formatTime(31536000);
+        assertThat(result, equalTo("1 year"));
+    }
+
+    @Test
+    void oneYearOneDayAndOneSecondTest() {
+        Challenge01 challenge01 = new Challenge01();
+        String result = challenge01.formatTime(31536000 + 86400 + 1 );
+        assertThat(result, equalTo("1 year, 1 day and 1 second"));
+    }
+
+    @Test
+    void twoYearsTwoDaysAndTwoSecondsTest() {
+        Challenge01 challenge01 = new Challenge01();
+        String result = challenge01.formatTime(( 31536000 * 2 ) + ( 86400 * 2 ) + 2 );
+        assertThat(result, equalTo("2 years, 2 days and 2 seconds"));
     }
 
 }
